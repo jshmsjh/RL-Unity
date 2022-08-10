abdd210e1ea2b153712845b7c07b650905bbdf79
jshmsjh <55683868+jshmsjh@users.noreply.github.com>
2022-08-10 20:07:39 +0800

    update unity environment

---
+++ Assets/Scripts/Car/CarUserControl.cs
@@ -0,0 +1,20 @@
+// Control the car manually for behavior learning
+using System;
+using UnityEngine;
+
+
+namespace UnityStandardAssets.Vehicles.Car
+{
+    [RequireComponent(typeof (CarController))]
+    public class CarUserControl : MonoBehaviour
+    {
+        private CarController m_Car; // the car controller we want to use
+
+        private void Awake()
+        {
+            // get the car controller
+            m_Car = GetComponent<CarController>();
+        }
+        
+    }
+}
